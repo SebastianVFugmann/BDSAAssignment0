6a11c829d279f1263efe57dec5e3b5f957db039a
Sebastian Fugmann <sefu@itu.dk>
2021-09-06 09:34:48 +0200

    Implemented error handling

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -2,14 +2,45 @@
 
 namespace LeapYear
 {
-    class Program
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please, type in a year and press [Enter]");
+            
+            var input = Console.ReadLine().Trim();
+            int number;
+            bool converted = int.TryParse(input, out number);
+            if(converted)
+            {
+                try
+                {
+                    bool result = IsLeapYear(number);
+                    if(result) Console.WriteLine("Yay");
+                    else Console.WriteLine("Nay");
+                } catch (YearToEarlyException e)
+                {
+                    Console.WriteLine("YearToEarlyException: " + e.Message);
+                }
+            } else
+            {
+                Console.WriteLine("Given input is not a year");
+            } 
+        }
+
         public static bool IsLeapYear(int year)
         {
+            if(year < 1582) throw (new YearToEarlyException(year + " is earlier than 1582, so it can't be a leap year"));
             if(year % 4 != 0) return false;
             if(year % 400 == 0) return true;
-            if(year % 100 == 0) return true;
+            if(year % 100 == 0) return false;
             return true;
         }
     }
+
+    public class YearToEarlyException: Exception 
+    {
+        public YearToEarlyException(string message): base(message) {
+        }
+}
 }
