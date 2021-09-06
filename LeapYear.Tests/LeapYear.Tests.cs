08dee780703bd57721aba18af759374946361a72
SebastianVFugmann <90041204+SebastianVFugmann@users.noreply.github.com>
2021-09-06 09:36:57 +0200

    Implemented GUI and error handling (#2)
    * Tested and implemented IsLeapYear
    
    * Implemented error handling
    
    Co-authored-by: Sebastian Fugmann <sefu@itu.dk>

---
+++ LeapYear.Tests/LeapYear.Tests.cs
@@ -0,0 +1,57 @@
+using System;
+using Xunit;
+using LeapYear;
+
+namespace LeapYear.Tests
+{
+    public class LeapYearTest
+    {
+        [Fact]
+        public void is1953LeapYearMod4()
+        {
+            //Arrange
+
+            //Act
+            bool result = Program.IsLeapYear(1953);
+
+            //Assert
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void is1900LeapYearMod4and100()
+        {
+            //Arrange
+
+            //Act
+            bool result = Program.IsLeapYear(1900);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void is2000LeapYearMod4and100and400()
+        {
+            //Arrange
+
+            //Act
+            bool result = Program.IsLeapYear(2000);
+
+            //Assert
+            Assert.True(result);
+        }
+        /*
+        [Fact]
+        public void is1500LeapYearExceptionIsThrown()
+        {
+            //Arrange
+
+            //Act
+            YearToEarlyException exception = Assert.Throws<YearToEarlyException>(Program.IsLeapYear(1800));
+
+            //The thrown exception can be used for even more detailed assertions.
+            Assert.Equal("YearToEarlyException: 1500 is earlier than 1582, so it can't be a leap year", exception.Message);
+        }*/
+    }
+}
